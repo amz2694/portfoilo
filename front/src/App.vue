4123f06047f8939f544b488e17765b083ecc5a66
amz2694 <alimiree.zade@gmail.com>
2022-07-19 03:11:01 +0430

    darkmode icon

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <header>
+    <div class="lamp" @click="togglenight">
+      <div class="lamp-item lamp-top"></div>
+      <div class="lamp-item lamp-middle"></div>
+      <div class="lamp-item lamp-bottom"></div>
+      <div class="lamp-item lamp-light"></div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -8,6 +15,12 @@
 export default {
   name: 'App',
   components: {},
+  methods : {
+    togglenight () {
+      document.getElementsByClassName('lamp-light')[0].classList.toggle('open')
+      document.querySelector('body').classList.toggle('dark')
+    }
+  }
 }
 </script>
 
