9cd017eff14c551710c15137c05facdb519e2e1f
amz2694 <alimiree.zade@gmail.com>
2022-07-23 04:21:19 +0430

    running express server

---
+++ back/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const PORT = process.env.PORT || 8000;
+
+// built-in middleware to handle urlencoded data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
